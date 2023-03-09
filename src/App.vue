d69da84b420d3fe0623b1b017d27443100c6548a
ilario-moreni <ilario2001.mor@gmail.com>
2023-03-09 21:16:08 +0100

    file initialize milestione 3

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+import ProjectMain from './components/ProjectMain.vue';
+export default {
+  components:{
+    ProjectMain,
+  }
+}
+</script>
+<template lang="">
+  <div>
+    <ProjectMain />
+  </div>
+</template>
+<style lang="">
+  
+</style>
---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ProjectMain',
+    data(){
+        return{
+            projects: [],
+            loading: true,
+            baseUrl: 'http://localhost:5173/',
+
+        }
+    },
+    /* methods(){
+        getProjects(){
+
+        }
+    } */
+}
+</script>
+<template lang="">
+    <div>
+        ciao
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
