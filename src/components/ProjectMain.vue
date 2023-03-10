9f39a6969d21f75a1015d6ffe48464c25eb8fb4a
ilario-moreni <ilario2001.mor@gmail.com>
2023-03-10 16:58:54 +0100

    milestone 4

--- src/components/ProjectMain.vue
+++ src/components/ProjectMain.vue
@@ -1,25 +1,57 @@
 <script>
+import axios from 'axios';
 export default {
     name: 'ProjectMain',
     data(){
         return{
             projects: [],
             loading: true,
-            baseUrl: 'http://localhost:5173/',
+            baseUrl: 'http://127.0.0.1:8000',
 
         }
     },
-    /* methods(){
+    methods:{
         getProjects(){
-
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                console.log(response);
+                this.projects = response.data.projects;
+                this.loading = false;
+            });
         }
-    } */
+    },
+    mounted(){
+        this.getProjects();
+    }
 }
 </script>
 <template lang="">
-    <div>
-        ciao
-    </div>
+    
+        <div v-if='loading'>
+            <div class="loader">
+
+            </div>
+        </div>
+        <div  v-else class='container'>
+
+            <div class='d-flex row g-3'>
+                <div v-for='project in projects' class="card card-body col-3 mx-2" :key="project.id">
+                    <div class="card-img-top">
+                        <img :src="projects.file_img != null ? projects.file_img : ''" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-title">
+                        {{project.title}}
+                    </div>
+                    <div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">{{project.budget}}&euro;</li>
+                            <li class="list-group-item">{{project.members_num}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </template>
 <style lang="scss" scoped>
     
